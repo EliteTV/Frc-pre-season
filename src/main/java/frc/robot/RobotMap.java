7e2b5ed90d233f5e7d9579f5ef70e6b1a53bee46
Connor Eames <elitetv2020@gmail.com>
2019-11-13 15:50:29 -0500

    First uploade

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  //public static int leftMotor = 1;
+  //public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+
+  // controller buttons
+    
+  public static final boolean xButton = false;
+	//PWM
+  //DIO
+  //CAN
+    //drive moters
+    public static int leftMasterPort = 0;
+    public static int leftSlavePort = 1;
+    public static int rightMasterPort = 2;
+    public static int rightSlavePort = 3;
+    public static final int intake = 4;
+
+  //USB
+    //drive joystck
+    public static final int driverController = 0;
+    
+    
+  //Button / Axxes
+    public static final int driveXButton = 3;
+	  public static final int leftStickY = 1;
+	public static final int leftStickX = 0;
+	public static final int rightStickX = 4;
+	
+
+  //Other
+    //Moter Speed
+    public static double maxMoveSpeed = .5;
+    public static double minMoveSpeed = -.5;
+
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubSystem.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.DriveManuleCommand;
+
+/**
+ * Add your docs here.
+ */
+public class DriveSubSystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  // create moters
+  private WPI_VictorSPX leftMaster = new WPI_VictorSPX(RobotMap.leftMasterPort);
+  private WPI_VictorSPX leftSlave = new WPI_VictorSPX(RobotMap.leftSlavePort);
+  private WPI_VictorSPX rightMaster = new WPI_VictorSPX(RobotMap.rightMasterPort);
+  private WPI_VictorSPX rightSlave = new WPI_VictorSPX(RobotMap.rightSlavePort); 
+
+
+
+  public DifferentialDrive drive = new DifferentialDrive(leftMaster, rightMaster);
+
+  //creat constructor function 
+  public DriveSubSystem() {
+    // setting slaves to follow masters 
+    leftSlave.follow(leftMaster);
+    rightSlave.follow(rightMaster);
+  }
+
+
+ 
+  public void manualDrive(double move, double turn){
+    
+    if(move > RobotMap.maxMoveSpeed) move = RobotMap.maxMoveSpeed;
+    if(move < RobotMap.minMoveSpeed) move = RobotMap.minMoveSpeed;
+    if(turn > RobotMap.maxMoveSpeed) move = RobotMap.maxMoveSpeed;
+    if(turn < RobotMap.minMoveSpeed) move = RobotMap.minMoveSpeed;
+
+    drive.arcadeDrive(move, turn);
+  }
+  
+
+
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new DriveManuleCommand());
+  }
+}
